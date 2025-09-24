cfaff30bf611686e2bf194f9e67c1545fc54cea7
Artem Yadelskyi <mymmrac@gmail.com>
2025-09-24 23:22:31 +0300

    Project get handlers

--- pkg/handler/project/handler.go
+++ pkg/handler/project/handler.go
@@ -22,9 +22,58 @@ func RegisterHandlers(router fiber.Router, projectRepository project.Repository)
 
 	api := router.Group("/api/project", auth.RequireMiddleware)
 
+	api.Get("/", h.getAllHandler)
+	api.Get("/:projectID", h.getHandler)
 	api.Post("/", h.createHandler)
 	api.Put("/", h.updateHandler)
 	api.Delete("/", h.deleteHandler)
+}
+
+type projectInfo struct {
+	ID   id.ID  `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h *handler) getAllHandler(fCtx fiber.Ctx) error {
+	projects, err := h.projectRepository.GetByOwnerID(fCtx, auth.MustUserFromContext(fCtx).ID)
+	if err != nil {
+		logger.FromContext(fCtx).Errorw("get projects", "error", err)
+		return fiber.NewError(fiber.StatusInternalServerError)
+	}
+
+	response := make([]projectInfo, len(projects))
+	for i, model := range projects {
+		response[i] = projectInfo{
+			ID:   model.ID,
+			Name: model.Name,
+		}
+	}
+
+	return fCtx.JSON(response)
+}
+
+func (h *handler) getHandler(fCtx fiber.Ctx) error {
+	var request struct {
+		ID id.ID `uri:"projectID" validate:"required"`
+	}
+
+	if err := fCtx.Bind().URI(&request); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest)
+	}
+
+	projectModel, found, err := h.projectRepository.GetByID(fCtx, request.ID, auth.MustUserFromContext(fCtx).ID)
+	if err != nil {
+		logger.FromContext(fCtx).Errorw("get project", "error", err)
+		return fiber.NewError(fiber.StatusInternalServerError)
+	}
+	if !found {
+		return fiber.NewError(fiber.StatusNotFound)
+	}
+
+	return fCtx.JSON(&projectInfo{
+		ID:   projectModel.ID,
+		Name: projectModel.Name,
+	})
 }
 
 func (h *handler) createHandler(fCtx fiber.Ctx) error {
